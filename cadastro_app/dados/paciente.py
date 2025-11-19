744e51d90c0ad52b6706628b6e82163bbd486cc2
savioguilherme <sguilhermerodri015@gmail.com>
2025-11-19 14:29:01 -0300

    correção nome de uma váriável

--- cadastro_app/dados/paciente.py
+++ cadastro_app/dados/paciente.py
@@ -4,10 +4,10 @@ class Paciente(Pessoa):
 
     '''Classe que representa um paciente'''
 
-    def __init__(self, id_paciente, nome_paciente, data_nascimento, data_incial, tipo = "paciente"):
+    def __init__(self, id_paciente, nome_paciente, data_nascimento, data_inicial, tipo = "paciente"):
         super().__init__(id_paciente, nome_paciente, tipo)
         self.data_nascimento = data_nascimento
-        self.data_inicial = data_incial
+        self.data_inicial = data_inicial
     
     def __repr__(self):
         return f"Paciente(id={self.id_pessoa}, nome='{self.nome}')"
