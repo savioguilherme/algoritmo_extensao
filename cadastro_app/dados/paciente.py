a517e23505a51895d615caa8a1e5014a32f85c56
savioguilherme <sguilhermerodri015@gmail.com>
2025-11-22 15:52:34 -0300

    criação da classe sessao e de uma lista de sessoes com seu método de adicionar na classe paciente

--- cadastro_app/dados/paciente.py
+++ cadastro_app/dados/paciente.py
@@ -1,15 +1,20 @@
 from dados.pessoa import Pessoa
+from dados.sessao import Sessao
 
 class Paciente(Pessoa):
 
     '''Classe que representa um paciente'''
 
-    def __init__(self, id_paciente, nome_paciente, data_nascimento, data_inicial, pesquisador, fisioterapeuta,  tipo = "paciente"):
+    def __init__(self, id_paciente, nome_paciente, data_nascimento, data_inicial, pesquisador, fisioterapeuta, tipo = "paciente"):
         super().__init__(id_paciente, nome_paciente, tipo)
         self.data_nascimento = data_nascimento
         self.data_inicial = data_inicial
         self.pesquisador_responsavel = pesquisador
         self.fisioterapeuta_responsavel = fisioterapeuta
-    
+        self.sessoes = []
+
+    def adicionarSessao(self, cod, horario):
+        self.sessoes.append(Sessao(cod, horario, self.pesquisador_responsavel, self.fisioterapeuta_responsavel, self.nome)) 
+
     def __repr__(self):
         return f"Paciente(id={self.id_pessoa}, nome='{self.nome}')"
---
+++ cadastro_app/dados/sessao.py
@@ -0,0 +1,7 @@
+class Sessao():
+    def __init__(self, codigo, horario, pesquisador, fisioterapeuta, paciente):
+        self.codigo = codigo
+        self.horario = horario
+        self.pesquisador = pesquisador
+        self.fisioterapeuta = fisioterapeuta
+        self.paciente = paciente
