209827f07c5982b9262d778245ed0110abe2a984
savioguilherme <sguilhermerodri015@gmail.com>
2025-12-05 13:57:59 -0300

    Inserção de novos métodos, ainda em desenvolvimento

--- cadastro_app/interfacegrafica/base_widgets.py
+++ cadastro_app/interfacegrafica/base_widgets.py
@@ -1,6 +1,9 @@
 import customtkinter as ctk
 
 class BaseWidgets():
+
+    '''Classe que fornece uma padrão para todos os widgets a serem utilizados a fim de evitar redundancias no codigo'''
+
     def __init__(self):
         pass
     def label(self, janela, texto, cor):
@@ -18,4 +21,42 @@ class BaseWidgets():
             #justify="center",
             padx=1,
             pady=1,
+        )
+    def entry(self, janela, texto, cor):
+        return ctk.CTkEntry(
+            master=janela,
+            #textvariable=texto_var,
+            text=texto,
+            width=200,
+            height=50,
+            corner_radius=10,
+            fg_color=cor,
+            text_color="black",
+            #placeholder_text_color=,
+            #placeholder_text=,
+            font=("Arial", 20, "bold"),
+            state="normal",
+        )
+    def button(self, janela, texto, cor):
+        return ctk.CTkButton(
+            master=janela,
+            width=200,
+            height=50,
+            corner_radius=10,
+            #border_width=,
+            #border_spacing=,
+            fg_color=cor,
+            #hover_color=,
+            #border_color=,
+            #text_color="#0077ff",
+            #placeholder_text_color=,
+            #placeholder_text=,
+            #textvariable=texto_var,
+            text=texto,
+            #font=,
+            #state=,
+            #hover=,
+            #command=,
+            #compound=,
+            #anchor=,
         )
