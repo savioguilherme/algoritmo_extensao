8f9e26926a6be4e8131599d0e3c9757913eff905
savioguilherme <sguilhermerodri015@gmail.com>
2025-12-08 17:58:58 -0300

    utilizando a classe BaseWidgets

--- cadastro_app/interfacegrafica/menu_pesquisador.py
+++ cadastro_app/interfacegrafica/menu_pesquisador.py
@@ -1,4 +1,3 @@
-import customtkinter
 from interfacegrafica.base_frame import BaseFrame
 from interfacegrafica.base_widgets import BaseWidgets
 
@@ -15,14 +14,14 @@ class MenuPesquisador(BaseFrame):
         self.abrir_restricoes = abrir_restricoes
         self.widgets = BaseWidgets()
 
-        self.btn = customtkinter.CTkButton(self.container, text="Cadastrar Paciente", width=250, height=40, command=self.cadastro_paciente, font=("Arial", 20, "bold"))
-        self.btn.grid(row=1, column=0, padx=10, pady=10)
+        self.btn_cadastrar_paci = self.widgets.button(self.container, texto="Cadastrar Paciente", comando=self.cadastro_paciente, cor="blue")
+        self.btn_cadastrar_paci.grid(row=1, column=0, padx=10, pady=10)
 
-        self.btn_agenda = customtkinter.CTkButton(self.container, text="Agenda", width=250, height=40, command=self.abrir_agenda, font=("Arial", 20, "bold"))
+        self.btn_agenda = self.widgets.button(self.container, texto="Agenda", comando=self.abrir_agenda, cor="blue")
         self.btn_agenda.grid(row=1, column=1, padx=10, pady=10)
 
         self.btn_restricoes = self.widgets.button(self.container, texto="Cadastrar Restrições Agenda", comando=self.abrir_restricoes, cor="blue")
         self.btn_restricoes.grid(row=1, column=2, sticky="w", padx=10, pady=10)
 
-        self.btn_voltar = customtkinter.CTkButton(self.container, text="Sair", width=250, height=40, command=self.voltar_callback, font=("Arial", 20, "bold"), fg_color="red")
+        self.btn_voltar = self.widgets.button(self.container, texto="Sair", comando=self.voltar_callback, cor="red")
         self.btn_voltar.grid(row=2, column=2, padx=10, pady=10)
