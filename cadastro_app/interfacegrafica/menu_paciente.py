9930ed93ce5b4bbfdfbf5af050ab85ece46b3013
savioguilherme <sguilhermerodri015@gmail.com>
2025-12-09 14:27:09 -0300

    Inicio da construção da classe menu paciente, para acessar os dados de um paciente específico bem como cadastrar suas restrições de horários

---
+++ cadastro_app/interfacegrafica/menu_paciente.py
@@ -0,0 +1,27 @@
+from interfacegrafica.base_frame import BaseFrame
+from interfacegrafica.base_widgets import BaseWidgets
+
+class MenuAdministrador(BaseFrame):
+     
+    """Menu do paciente"""
+
+    def __init__(self, master, voltar_callback):
+        super().__init__(master, titulo="Menu Paciente")
+
+        self.voltar_callback = voltar_callback
+        self.widgets = BaseWidgets()
+        
+        self.btn_fisioterapeuta = self.widgets.button(self.container, texto="Cadastrar Fisioterapeuta", comando=self.cadastro_fisioterapeuta, cor="blue")
+        self.btn_fisioterapeuta.grid(row=1, column=0, sticky="w", padx=10, pady=10)
+
+        self.btn_pesquisador = self.btn_cadastrar_paci = self.widgets.button(self.container, texto="Cadastrar Pesquisador", comando=self.cadastro_pesquisador, cor="blue")
+        self.btn_pesquisador.grid(row=1, column=1, sticky="w", padx=10, pady=10)
+
+        self.btn_listar_fisioterapeuta = self.widgets.button(self.container, texto="Listar Fisioterapeutas", comando=self.listar_fisioterapeutas, cor="blue")
+        self.btn_listar_fisioterapeuta.grid(row=1, column=2, sticky="w", padx=10, pady=10)
+
+        self.btn_listar_pesquisadores = self.widgets.button(self.container, texto="Listar Pesquisadores", comando=self.listar_pesquisadores, cor="blue")
+        self.btn_listar_pesquisadores.grid(row=1, column=3, sticky="w", padx=10, pady=10)
+
+        self.btn_voltar = self.widgets.button(self.container, texto="Sair", comando=self.voltar_callback, cor="red")
+        self.btn_voltar.grid(row=2, column=1, sticky="e", padx=10, pady=10)
