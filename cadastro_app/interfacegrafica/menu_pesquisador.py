824e349ee5686ff07cc0b248357a0c12ba4a11fe
savioguilherme <sguilhermerodri015@gmail.com>
2025-12-09 14:29:27 -0300

    Criação do botão para listar os pacientes cadastrados e acessar seu menu com os seus dados

--- cadastro_app/interfacegrafica/menu_pesquisador.py
+++ cadastro_app/interfacegrafica/menu_pesquisador.py
@@ -17,11 +17,14 @@ class MenuPesquisador(BaseFrame):
         self.btn_cadastrar_paci = self.widgets.button(self.container, texto="Cadastrar Paciente", comando=self.cadastro_paciente, cor="blue")
         self.btn_cadastrar_paci.grid(row=1, column=0, padx=10, pady=10)
 
+        self.btn_listar_paci = self.widgets.button(self.container, texto="Listar Pacientes", comando=None, cor="blue")
+        self.btn_listar_paci.grid(row=1, column=1, padx=10, pady=10)
+
         self.btn_agenda = self.widgets.button(self.container, texto="Agenda", comando=self.abrir_agenda, cor="blue")
-        self.btn_agenda.grid(row=1, column=1, padx=10, pady=10)
+        self.btn_agenda.grid(row=1, column=2, padx=10, pady=10)
 
         self.btn_restricoes = self.widgets.button(self.container, texto="Cadastrar Restrições Agenda", comando=self.abrir_restricoes, cor="blue")
-        self.btn_restricoes.grid(row=1, column=2, sticky="w", padx=10, pady=10)
+        self.btn_restricoes.grid(row=1, column=3, sticky="w", padx=10, pady=10)
 
         self.btn_voltar = self.widgets.button(self.container, texto="Sair", comando=self.voltar_callback, cor="red")
         self.btn_voltar.grid(row=2, column=2, padx=10, pady=10)
