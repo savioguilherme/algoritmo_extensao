7369997745b17c269bcb5a9435446b85c2d19e70
savioguilherme <sguilhermerodri015@gmail.com>
2025-12-10 15:35:54 -0300

    Ajustes nas janelas

--- cadastro_app/interfacegrafica/base_frame.py
+++ cadastro_app/interfacegrafica/base_frame.py
@@ -7,18 +7,17 @@ class BaseFrame(customtkinter.CTkFrame):
     def __init__(self, master, titulo=""):
         super().__init__(master)
 
-        self.grid(row=0, column=0, padx=40, pady=40, sticky="nsew")
-
-        #Faz o frame ocupar toda a janela
+        #frame de fundo
+        self.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
         self.grid_columnconfigure(0, weight=1)
-        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=0)
 
-        #Container central para colocar widgets das subclasses
+        # frame principal onde são construidos os widgets das subclasses
         self.container = customtkinter.CTkFrame(self, fg_color="transparent")
-        self.container.grid(row=0, column=0, padx=20, pady=20, sticky="n")
+        self.container.grid(row=0, column=0, padx=20, pady=20)
         self.container.grid_columnconfigure((0,1,2,3), weight=1)
-        self.container.grid_rowconfigure((0,1,2,3), weight=1)
+        self.container.grid_rowconfigure((0,1,2,3), weight=0)
 
         if titulo:
-            self.label_titulo = customtkinter.CTkLabel(self.container, text=titulo, width=250, height=40, font=("Arial", 40, "bold"))
-            self.label_titulo.grid(row=0, column=0, columnspan=4, padx=20, pady=(20,40), sticky="nsew")
+            self.label_titulo = customtkinter.CTkLabel(self.container, text=titulo, width=250, height=40, font=("Arial", 45, "bold"))
+            self.label_titulo.grid(row=0, column=0, columnspan=4, padx=20, pady=(10,20), sticky="nsew")
