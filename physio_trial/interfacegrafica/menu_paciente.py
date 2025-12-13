0b8195b0fa6bd006151ef5eb867eccb33ed1989e
savioguilherme <sguilhermerodri015@gmail.com>
2025-12-13 17:22:55 -0300

    criação de campos para exibir informações de um paciente específico

--- physio_trial/interfacegrafica/menu_paciente.py
+++ physio_trial/interfacegrafica/menu_paciente.py
@@ -2,7 +2,6 @@ from interfacegrafica.base_frame import BaseFrame
 from interfacegrafica.base_widgets import BaseWidgets
 
 class MenuPaciente(BaseFrame):
-     
     """Menu do paciente"""
 
     def __init__(self, master, voltar_callback):
@@ -12,20 +11,23 @@ class MenuPaciente(BaseFrame):
         self.widgets = BaseWidgets()
 
         #configurando o frame
-        self.grid_rowconfigure((1,2,3), weight=0)
-        self.grid_columnconfigure(2, weight=1)
+        self.grid_rowconfigure((1,2,3,4,5,6), weight=0)
+        self.grid_columnconfigure((1,2,3), weight=1)
         
-        #self.btn_fisioterapeuta = self.widgets.button(self, texto="Cadastrar Fisioterapeuta", comando=self.cadastro_fisioterapeuta, cor="blue")
-        #self.btn_fisioterapeuta.grid(row=1, column=0, sticky="w", padx=10, pady=10)
+        self.label_nome = self.widgets.label(janela=self, texto="Nome do Paciente", cor="transparent")
+        self.label_nome.grid(row=1, column=1, sticky="e", padx=(20,20), pady=(20,10))
 
-        #self.btn_pesquisador = self.btn_cadastrar_paci = self.widgets.button(self, texto="Cadastrar Pesquisador", comando=self.cadastro_pesquisador, cor="blue")
-        #self.btn_pesquisador.grid(row=1, column=1, sticky="w", padx=10, pady=10)
+        self.label_fisioterapeuta = self.widgets.label(janela=self, texto="Nome do fisio", cor="transparent")
+        self.label_fisioterapeuta.grid(row=2, column=1, sticky="e", padx=(20,20), pady=(10,10))
+        
+        self.label_pesquisador = self.widgets.label(janela=self, texto="Nome do pesqui", cor="transparent")
+        self.label_pesquisador.grid(row=3, column=1, sticky="e", padx=(20,20), pady=(10,20))
 
-        #self.btn_listar_fisioterapeuta = self.widgets.button(self, texto="Listar Fisioterapeutas", comando=self.listar_fisioterapeutas, cor="blue")
-        #self.btn_listar_fisioterapeuta.grid(row=1, column=2, sticky="w", padx=10, pady=10)
+        self.label_status = self.widgets.label(janela=self, texto="ativo/não-ativo", cor="transparent")
+        self.label_status.grid(row=4, column=1, sticky="e", padx=(20,20), pady=(10,20))
 
-        #self.btn_listar_pesquisadores = self.widgets.button(self, texto="Listar Pesquisadores", comando=self.listar_pesquisadores, cor="blue")
-        #self.btn_listar_pesquisadores.grid(row=1, column=3, sticky="w", padx=10, pady=10)
+        self.label_sessao = self.widgets.label(janela=self, texto="sessao tal", cor="transparent")
+        self.label_sessao.grid(row=5, column=1, sticky="e", padx=(20,20), pady=(10,20))
 
         self.btn_voltar = self.widgets.button(self, texto="Voltar", comando=self.voltar_callback, cor="red")
-        self.btn_voltar.grid(row=2, column=1, sticky="e", padx=20, pady=20)
+        self.btn_voltar.grid(row=6, column=1, sticky="e", padx=20, pady=20)
