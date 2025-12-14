bd547f20e2a2d77f84c1e6961257da399d90a274
savioguilherme <sguilhermerodri015@gmail.com>
2025-12-14 14:18:20 -0300

    Criação da classe restrições

---
+++ physio_trial/dados/restricoes_dias_horarios.py
@@ -0,0 +1,35 @@
+from calendar import Calendar
+from datetime import *
+
+class RestricoesDiasHorarios():
+    '''Classe que armazena as restricoes de um tipo de pessoa'''
+    
+    def __init__(self):
+        self.disponibilidade_semanal = [set() for _ in range(7)]
+        self.restricoes = set()
+    
+    def adicionar_disponibilidade(self, dia_semana, horario):
+        self.disponibilidade_semanal[dia_semana%7].add(horario)
+
+    def remover_disponibilidade(self, dia_semana, horario):
+        self.disponibilidade_semanal[dia_semana%7].discard(horario)
+
+    def adicionar_restricao(self, dia_horario):
+        self.restricoes.add(dia_horario)
+
+    def remover_restricao(self, dia_horario):
+        self.restricoes.discard(dia_horario)
+
+    def esta_disponivel(self, dia_horario):
+        dia_semana = dia_horario.weekday()
+        horario = dia_horario.time()
+        return horario in self.disponibilidade_semanal[dia_semana] and not dia_horario in self.restricoes
+
+    def get_lista_disponibilidade_semanal(self):
+        return [list(disponibilidade) for disponibilidade in self.disponibilidade_semanal]
+
+    def get_lista_restricoes(self):
+        return list(self.restricoes)
+    
+    def get_horarios(self):
+        return set.union(*[horarios for horarios in self.disponibilidade_semanal])
