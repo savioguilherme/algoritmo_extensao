854e55a72f027038539e48cb5b0798e7b93954d1
savioguilherme <sguilhermerodri015@gmail.com>
2025-12-14 13:23:11 -0300

    classe que listas todos os pacientes ativos

--- physio_trial/interfacegrafica/listar_pacientes.py
+++ physio_trial/interfacegrafica/listar_pacientes.py
@@ -8,27 +8,27 @@ class ListarPacientes(BaseFrame):
         super().__init__(master, "Lista de Pacientes")
 
         self.pacientes = pacientes
-        self.voltar_callback = voltar_callback
+        self.voltar_callback = voltar_callback 
         self.abrir_menu_paciente = abrir_menu_paciente
         self.widgets = BaseWidgets()
 
-        self._configurar_layout()
-        self._criar_lista_pacientes()
-        self._criar_botao_voltar()
-
-    def _configurar_layout(self):
+        #configurando o frame
+        self.grid_rowconfigure((1,2), weight=0)
         self.grid_columnconfigure((1,2), weight=1)
+        self._criar_lista_pacientes()
 
     def _criar_lista_pacientes(self):
         if not self.pacientes:
             label = self.widgets.label(self, texto="Nenhum paciente cadastrado.", cor="transparent")
             label.grid(row=1, column=1, columnspan=3, pady=20)
-            return
+            return self._criar_botao_voltar(2)
 
         for i, paciente in enumerate(self.pacientes):
             btn = self.widgets.button(self, texto=paciente.nome, comando=lambda p=paciente: self.abrir_menu_paciente(p), cor="blue")
-            btn.grid(row=i+1, column=2, sticky="ew", pady=5, padx=20)
+            btn.grid(row=i, column=1, sticky="e", columnspan=3, padx=20, pady=5)
 
-    def _criar_botao_voltar(self):
+        return self._criar_botao_voltar(i)
+    
+    def _criar_botao_voltar(self, i):
         btn_voltar = self.widgets.button(self, texto="Voltar", comando=self.voltar_callback, cor="red")
-        btn_voltar.grid(row=99, column=1, pady=20)
+        btn_voltar.grid(row=i, column=1, sticky="e", padx=(20,20), pady=(10,20))
