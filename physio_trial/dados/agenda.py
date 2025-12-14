4af13614044808953a327e02aab1c03a9ccb6967
savioguilherme <sguilhermerodri015@gmail.com>
2025-12-14 14:07:24 -0300

    Novos métodos para lidar com a sessoes

--- physio_trial/dados/agenda.py
+++ physio_trial/dados/agenda.py
@@ -1,17 +1,23 @@
 from collections import deque
+from dados.paciente import Paciente
 
 class Agenda():
-
-    ''''''
+    '''Classe que recebe todos os pacientes ativos e suas sessoes, e se relaciona com a heuristica'''
 
     def __init__(self):
-        self.sessoes_pacientes_ativos_nao_agendadas = deque([])
+        self.sessoes_pacientes_ativos_nao_agendadas = deque([]) 
         self.sessoes_agendadas = deque([])
     
     def buscar_sessoes_pacientes_ativos(self):
         pass
     
     def agendar_sessao(self):
+        pass
+    
+    def concluir_sessao(self):
+        pass
+
+    def remarcar_sessao(self): 
         pass
     
     '''def __init__(self):
--- physio_trial/dados/paciente.py
+++ physio_trial/dados/paciente.py
@@ -1,9 +1,7 @@
 from dados.pessoa import Pessoa
 from dados.sessao import Sessao
-from dados.agenda import Agenda
 
 class Paciente(Pessoa):
-
     '''Classe que representa um paciente'''
 
     CODIGOS_SESSOES = [
@@ -17,25 +15,40 @@ class Paciente(Pessoa):
 
         self.pesquisador_responsavel = pesquisador
         self.fisioterapeuta_responsavel = fisioterapeuta
+
         self.sessoes_paciente = [
-            Sessao(cod, None, None, False, False)  #List Comprehension Python 
+            Sessao(None,cod, None, None, False, False)  #List Comprehension Python 
             for cod in self.CODIGOS_SESSOES
         ]
+
         self.horarios_restricao = []
         self.dias_restricao = []
+
+        self.conclusao = False
+        self.abandono = False
 
     def restricao_paciente(self, horarios, dias):
         self.horarios_restricao.append(horarios)
         self.dias_restricao.append(dias)
     
-    def verifica_conclusao(self):
-        pass
+    def verificar_conclusao_pesquisa(self):
+        for i in self.sessoes_paciente:
+            if i.conclusao is False:
+                return
+            self.conclusao = True
     
-    def verifica_abandono(self):
-        pass
+    def cadastrar_abandono_pesquisa(self):
+        self.abandono = True
+        self.desabilitar_paciente()
     
-    def desabilita_paciente(self): 
-        pass
+    def desabilitar_paciente(self): 
+        self.status_pessoa = False
+
+    def alterar_pesquisador(self, pesquisador): 
+        self.pesquisador_responsavel = pesquisador
+
+    def alterar_fisioterapeuta(self, fisioterapeuta): 
+        self.fisioterapeuta_responsavel= fisioterapeuta
 
     def __repr__(self):
         return f"Paciente(id={self.id_pessoa}, nome='{self.nome}')"
