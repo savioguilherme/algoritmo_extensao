18368357fca5d5b2904da3de80c2dd7056cfed34
savioguilherme <sguilhermerodri015@gmail.com>
2025-12-15 15:15:54 -0300

    correcoes  dos metodos

--- physio_trial/dados/agenda.py
+++ physio_trial/dados/agenda.py
@@ -15,28 +15,11 @@ class Agenda():
         return [p for p in self.pacientes if p.status_pessoa == True]
 
     def buscar_sessoes_pacientes_ativos(self):
-        sessoes = []
         for paciente in self.buscar_pacientes_ativos():
             for sessao in paciente.sessoes_paciente:
                 if not sessao.status_agendamento and not sessao.conclusao:
-                    sessoes.append(sessao)
-        return sessoes
-    
-    def buscar_sessoes_nao_agendadas_paciente(self, paciente):
-        return [
-            sessao for sessao in paciente.sessoes_paciente
-            if not sessao.status_agendamento and not sessao.conclusao
-        ]
-
-    def buscar_sessoes_pacientes_ativos(self):
-        self.sessoes_pacientes_ativos_nao_agendadas.clear()
-        pacientes_ativos = self.buscar_pacientes_ativos()
-        for paciente in pacientes_ativos:
-            sessoes_nao_agendadas = self.buscar_sessoes_nao_agendadas_paciente(paciente)
-
-        for sessao in sessoes_nao_agendadas:
-            self.sessoes_pacientes_ativos_nao_agendadas.append(sessao)
-
+                    self.sessoes_pacientes_ativos_nao_agendadas.append(sessao)
+                    
     def buscar_fisioterapeutas_ativos(self):
         pass
 
