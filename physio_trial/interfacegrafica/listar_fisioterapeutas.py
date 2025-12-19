22650e61a13e77b4554dee1e820a511467599e93
savioguilherme <sguilhermerodri015@gmail.com>
2025-12-19 15:09:57 -0300

    apagando classes e métodos arquivo texto

--- physio_trial/interfacegrafica/listar_fisioterapeutas.py
+++ physio_trial/interfacegrafica/listar_fisioterapeutas.py
@@ -1,7 +1,5 @@
 import customtkinter
 from interfacegrafica.base_frame import BaseFrame
-from armazenamento.armazenamento import Armazenamento
-from tkinter import messagebox
 
 class ListarFisioterapeutas(BaseFrame):
 
@@ -11,8 +9,6 @@ class ListarFisioterapeutas(BaseFrame):
         super().__init__(master, "Lista de Fisioterapeutas")
 
         self.voltar_callback = voltar_callback
-        self.guardar = Armazenamento()
-        self.texto_fisioterapeutas = self.exibir_fisioterapeutas()
 
         #configurando o frame
         self.grid_rowconfigure((1,2,3), weight=0)
@@ -23,24 +19,3 @@ class ListarFisioterapeutas(BaseFrame):
 
         self.btn_voltar = customtkinter.CTkButton(self, text="Voltar", command=self.voltar_callback, font=("Arial", 20, "bold"), fg_color="red")
         self.btn_voltar.grid(row=3, column=1, padx=20, pady=20)
-
-    def exibir_fisioterapeutas(self):
-        
-        """Carrega e retorna o texto formatado com os fisioterapeutas cadastrados."""
-
-        try:
-            df = self.guardar.carregar("fisioterapeutas")
-
-            if df.empty:
-                return "Nenhum fisioterapeuta cadastrado."
-
-            texto = "Fisioterapeutas cadastrados:\n\n"
-            for i, row in df.iterrows():
-                nome = row.get("Nome", "—")
-                texto += f"• {nome} \n"
-
-            return texto.strip()
-
-        except Exception as e:
-            messagebox.showerror("Erro", f"Erro ao carregar fisioterapeutas:\n{e}")
-            return "Erro ao carregar dados."
--- physio_trial/interfacegrafica/listar_pesquisadores.py
+++ physio_trial/interfacegrafica/listar_pesquisadores.py
@@ -1,7 +1,5 @@
 import customtkinter
 from interfacegrafica.base_frame import BaseFrame
-from armazenamento.armazenamento import Armazenamento
-from tkinter import messagebox
 
 class ListarPesquisadores(BaseFrame):
 
@@ -11,8 +9,6 @@ class ListarPesquisadores(BaseFrame):
         super().__init__(master, "Lista de Pesquisadores")
 
         self.voltar_callback = voltar_callback
-        self.guardar = Armazenamento()
-        self.texto_pesquisadores = self.exibir_pesquisadores()
 
         #configurando o frame
         self.grid_rowconfigure((1,2,3), weight=0)
@@ -23,25 +19,3 @@ class ListarPesquisadores(BaseFrame):
 
         self.btn_voltar = customtkinter.CTkButton(self, text="Voltar", command=self.voltar_callback, font=("Arial", 20, "bold"), fg_color="red")
         self.btn_voltar.grid(row=3, column=1, padx=20, pady=20)
-
-    def exibir_pesquisadores(self):
-        
-        """Carrega e retorna o texto formatado com os fisioterapeutas cadastrados."""
-
-        try:
-            df = self.guardar.carregar("pesquisadores")
-
-            if df.empty:
-                return "Nenhum pesquisador cadastrado."
-
-            texto = "Pesquisadores cadastrados:\n\n"
-            for i, row in df.iterrows():
-                nome = row.get("Nome", "—")
-                texto += f"• {nome} \n"
-
-            return texto.strip()
-
-        except Exception as e:
-            messagebox.showerror("Erro", f"Erro ao carregar pesquisadores:\n{e}")
-            return "Erro ao carregar dados."
-            
