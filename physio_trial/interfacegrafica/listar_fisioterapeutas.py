e7b93bbc60373c660c109fa0dc33d602eb67768e
saviorodrigues <sguilhermerodri015@gmail.com>
2026-01-17 14:50:16 -0300

    Classe de listagem finalizada, necessario mais testes

--- physio_trial/interfacegrafica/listar_fisioterapeutas.py
+++ physio_trial/interfacegrafica/listar_fisioterapeutas.py
@@ -1,22 +1,24 @@
 from inject import autoparams
 from interfacegrafica.base_frame import BaseFrame
 from interfacegrafica.base_widgets import BaseWidgets
-from dados.fisioterapeuta import Fisioterapeuta
-from interfacegrafica.menu_atualiza_dados_usuario import MenuAtualizaDadosUsuario
-from CTkMessagebox import CTkMessagebox
 from armazenamento.services.base.base_usuario_service import BaseUsuarioService
 from armazenamento.context.app_context import current_user_types_list
+from CTkMessagebox import CTkMessagebox
+from dados.fisioterapeuta import Fisioterapeuta
+from interfacegrafica.menu_atualiza_dados_usuario import MenuAtualizaDadosUsuario
 
 class ListarFisioterapeutas(BaseFrame):
 
     '''Lista dos os Fisioterapeutas do sistema'''
 
     @autoparams()
-    def __init__(self, master, voltar_callback, usuario_service: BaseUsuarioService):
+    def __init__(self, master, voltar_callback, abrir_menu_atualiza_dados, usuario_service: BaseUsuarioService):
         super().__init__(master, "Lista de Fisioterapeutas")
 
-        self.voltar_callback = voltar_callback
         self.widgets = BaseWidgets()
+        self.voltar_callback = voltar_callback
+        self.abrir_menu_atualiza_dados = abrir_menu_atualiza_dados
+        
         self.usuario_service = usuario_service
 
         # Dados dos fisioterapeutas
@@ -37,7 +39,7 @@ class ListarFisioterapeutas(BaseFrame):
             self.fisioterapeutas = self.usuario_service.listar_usuarios(lista_tipos=[fisioterapeuta_tipo_id], apenas_ativos=False)
             
             for i, fisio in enumerate(self.fisioterapeutas):
-                self.bnt_nome = self.widgets.button(self, texto=fisio.nome, comando=None, cor="Green")
+                self.bnt_nome = self.widgets.button(self, texto=fisio.nome, comando=lambda f_id=fisio.id_pessoa: self.abrir_menu_atualiza_dados(f_id), cor="Green")
                 self.bnt_nome.grid(row=i+1, column=1, sticky="e", padx=(20,10), pady=(10,20))
             return self._criar_botao_voltar(i)
         
@@ -47,7 +49,7 @@ class ListarFisioterapeutas(BaseFrame):
                 message=f"Erro ao carregar dados do usuário: {str(e)}", 
                 icon="cancel"
             ).get()
-            return None
+            self.voltar_callback()
 
     def _criar_botao_voltar(self, i):
         btn_voltar = self.widgets.button(self, texto="Voltar", comando=self.voltar_callback, cor="red")
