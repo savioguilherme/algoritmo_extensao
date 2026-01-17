b530c0cf1231cbea0cd3d4734525614c6b6fc522
saviorodrigues <sguilhermerodri015@gmail.com>
2026-01-17 14:50:27 -0300

    Classe de listagem finalizada, necessario mais testes

--- physio_trial/interfacegrafica/listar_pesquisadores.py
+++ physio_trial/interfacegrafica/listar_pesquisadores.py
@@ -1,21 +1,23 @@
+from inject import autoparams
 from interfacegrafica.base_frame import BaseFrame
 from interfacegrafica.base_widgets import BaseWidgets
-from dados.pesquisador import Pesquisador
-from inject import autoparams
 from armazenamento.services.base.base_usuario_service import BaseUsuarioService
 from armazenamento.context.app_context import current_user_types_list
+from CTkMessagebox import CTkMessagebox
+from dados.pesquisador import Pesquisador
 
 class ListarPesquisadores(BaseFrame):
 
     ''''Lista dos os Pesquisadores do sistema'''
 
     @autoparams()
-    def __init__(self, master, voltar_callback, usuario_service: BaseUsuarioService):
+    def __init__(self, master, voltar_callback, abrir_menu_atualiza_dados, usuario_service: BaseUsuarioService):
         super().__init__(master, "Lista de Pesquisadores")
 
-        self.voltar_callback = voltar_callback
         self.widgets = BaseWidgets()
+        self.voltar_callback = voltar_callback
         self.usuario_service = usuario_service
+        self.abrir_menu_atualiza_dados = abrir_menu_atualiza_dados
 
         # Dados dos pesquisadores
         self.pesquisadores: list[Pesquisador] = []
@@ -34,19 +36,18 @@ class ListarPesquisadores(BaseFrame):
                 
             self.pesquisadores = self.usuario_service.listar_usuarios(lista_tipos=[pesquisador_tipo_id], apenas_ativos=False)
             
-            if not self.pesquisadores:
-                self.label_vazio = self.widgets.label(self, texto="Nenhum pesquisador encontrado", cor="transparent")
-                self.label_vazio.grid(row=1, column=1, sticky="e", padx=(20,10), pady=(20,10))
-                return self._criar_botao_voltar(2)
-            
             for i, pesq in enumerate(self.pesquisadores):
-                label = self.widgets.label(self, texto=pesq.nome, cor="transparent")
+                label = self.widgets.button(self, texto=pesq.nome, comando=lambda p_id=pesq.id_pessoa: self.abrir_menu_atualiza_dados(p_id), cor="Green")
                 label.grid(row=i+1, column=1, sticky="e", padx=(20,10), pady=(10,20))
             return self._criar_botao_voltar(i)
         
         except Exception as e:
-            self.label_erro = self.widgets.label(self, texto=f"Erro ao carregar pesquisadores: {str(e)}", cor="transparent")
-            self.label_erro.grid(row=1, column=1, padx=20, pady=20)
+            CTkMessagebox(
+                title="Erro", 
+                message=f"Erro ao carregar dados do usuário: {str(e)}", 
+                icon="cancel"
+            ).get()
+            self.voltar_callback()
     
     def _criar_botao_voltar(self, i):
         btn_voltar = self.widgets.button(self, texto="Voltar", comando=self.voltar_callback, cor="red")
