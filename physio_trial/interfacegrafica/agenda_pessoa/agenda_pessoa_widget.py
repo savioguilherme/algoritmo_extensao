608875b6a3353768da5664695274e3c6e8b539af
peo5 <pedromsimoesferreira@gmail.com>
2026-02-25 23:58:54 -0300

    Adicionados avisos de intervalo entre sessões
    Agora o widget de sessões mostra avisos para quando os intervalos ideais entre sessões não são cumpridos. Isso inclui também duas sessões em um mesmo horário para um profissional.

--- physio_trial/interfacegrafica/agenda_pessoa/agenda_pessoa_widget.py
+++ physio_trial/interfacegrafica/agenda_pessoa/agenda_pessoa_widget.py
@@ -1,7 +1,8 @@
 import customtkinter as ctk
 import inject
-from datetime import datetime, date, time
+from datetime import datetime, date, time, timedelta
 from CTkMessagebox import CTkMessagebox
+from typing import Dict, List
 
 from armazenamento.services.base.base_paciente_service import BasePacienteService
 from armazenamento.services.base.base_sessao_service import BaseSessaoService
@@ -41,13 +42,25 @@ class AgendaPessoaWidget(ctk.CTkFrame):
 
         self._carregar_sessoes()
 
+    def _enumerar_sessao(self, codigo: str) -> int:
+        """Retorna um valor numérico para ordenar as sessões na sequência correta."""
+        if not codigo:
+            return -1
+        if codigo.startswith('S'):
+            return int(codigo[1:])
+        elif codigo == 'F00':
+            return 9
+        elif codigo == 'F01':
+            return 10
+        return -1
+
     def _classificar_sessao(self, codigo_sessao: str) -> tuple[bool, bool]:
         """
         Verifica se a sessão é para um pesquisador ou fisioterapeuta.
         Retorna (is_pesquisador, is_fisioterapeuta).
         """
         if codigo_sessao:
-            if codigo_sessao.startswith("F") or codigo_sessao == "S08":
+            if codigo_sessao.startswith('F') or codigo_sessao == "S08":
                 return True, False  # Pesquisador
             return False, True  # Fisioterapeuta
         return False, False
@@ -56,72 +69,112 @@ class AgendaPessoaWidget(ctk.CTkFrame):
         """
         Carrega e filtra as sessões a serem exibidas, verificando conflitos de horário.
         """
-        # Limpa cards existentes antes de carregar novos
         for card in self.sessao_cards:
             card.destroy()
         self.sessao_cards = []
 
-        # Obter pacientes e sessões filtradas
-        pacientes = self.paciente_service.listar_pacientes(apenas_ativos=False)
+        pacientes = self.paciente_service.listar_pacientes(apenas_ativos=True)
         sessoes_filtradas: list[Sessao] = []
         profissionais_ids = set()
+        agenda_profissional: Dict[int, Dict[datetime, int]] = {}
+        sessoes_por_paciente: Dict[int, List[Sessao]] = {}
 
         for paciente in pacientes:
             if self.pacientes_ids is not None and paciente.id_pessoa not in self.pacientes_ids:
                 continue
 
             for sessao in paciente.sessoes_paciente:
-                is_pesquisador_session, is_fisio_session = self._classificar_sessao(sessao.codigo)
+                indice = self._enumerar_sessao(sessao.codigo)
 
-                print("sessao recebida {} {}: {} {}".format(sessao.paciente.id_pessoa, sessao.codigo, sessao.status_agendamento, sessao.conclusao)) #dbg
+                if indice >= 0:
+                    if not sessao.paciente.id_pessoa in sessoes_por_paciente:
+                        sessoes_por_paciente[sessao.paciente.id_pessoa] = [None] * 11
+                    sessoes_por_paciente[sessao.paciente.id_pessoa][indice] = sessao
 
-                passes_pesquisador_filter = True
-                if is_pesquisador_session and self.pesquisadores_ids is not None:
-                    if paciente.pesquisador_responsavel is None or \
-                       paciente.pesquisador_responsavel.id_pessoa not in self.pesquisadores_ids:
-                        passes_pesquisador_filter = False
+                is_pesq, is_fisio = self._classificar_sessao(sessao.codigo)
 
-                passes_fisioterapeuta_filter = True
-                if is_fisio_session and self.fisioterapeutas_ids is not None:
-                    if paciente.fisioterapeuta_responsavel is None or \
-                       paciente.fisioterapeuta_responsavel.id_pessoa not in self.fisioterapeutas_ids:
-                        passes_fisioterapeuta_filter = False
+                passes_pesq_filter = True
+                if is_pesq and self.pesquisadores_ids is not None and (paciente.pesquisador_responsavel is None or paciente.pesquisador_responsavel.id_pessoa not in self.pesquisadores_ids):
+                    passes_pesq_filter = False
 
-                if passes_pesquisador_filter and passes_fisioterapeuta_filter:
+                passes_fisio_filter = True
+                if is_fisio and self.fisioterapeutas_ids is not None and (paciente.fisioterapeuta_responsavel is None or paciente.fisioterapeuta_responsavel.id_pessoa not in self.fisioterapeutas_ids):
+                    passes_fisio_filter = False
+
+                if passes_pesq_filter and passes_fisio_filter:
                     sessoes_filtradas.append(sessao)
-                    if paciente.pesquisador_responsavel:
-                        profissionais_ids.add(paciente.pesquisador_responsavel.id_pessoa)
-                    if paciente.fisioterapeuta_responsavel:
-                        profissionais_ids.add(paciente.fisioterapeuta_responsavel.id_pessoa)
+                    prof_id = None
+                    if is_pesq and paciente.pesquisador_responsavel:
+                        prof_id = paciente.pesquisador_responsavel.id_pessoa
+                        profissionais_ids.add(prof_id)
+                    elif is_fisio and paciente.fisioterapeuta_responsavel:
+                        prof_id = paciente.fisioterapeuta_responsavel.id_pessoa
+                        profissionais_ids.add(prof_id)
 
-        # Obter dados completos dos profissionais com suas restrições
+                    if prof_id and sessao.dia and sessao.horario:
+                        dia_horario = datetime.combine(sessao.dia, sessao.horario)
+                        if prof_id not in agenda_profissional:
+                            agenda_profissional[prof_id] = {}
+                        if dia_horario not in agenda_profissional[prof_id]:
+                            agenda_profissional[prof_id][dia_horario] = 0
+                        agenda_profissional[prof_id][dia_horario] += 1
+
         profissionais = {uid: self.usuario_service.consultar(uid) for uid in profissionais_ids}
-
-        # Ordenar sessoes e criar cards com avisos
         sessoes_filtradas.sort(key=lambda s: (s.dia or date.max, s.horario or time.max))
 
         for sessao in sessoes_filtradas:
             avisos = []
-            if sessao.dia and sessao.horario:
-                dia_horario = datetime.combine(sessao.dia, sessao.horario)
+            dia_horario = datetime.combine(sessao.dia, sessao.horario) if sessao.dia and sessao.horario else None
 
-                # Checar disponibilidade do paciente
+            if dia_horario:
                 if not sessao.paciente.restricoes_paciente.esta_disponivel(dia_horario):
-                    avisos.append(f"Conflito de horário para o paciente.")
+                    avisos.append("Conflito de horário para o paciente.")
 
-                is_pesquisador_session, is_fisio_session = self._classificar_sessao(sessao.codigo)
+                is_pesq, is_fisio = self._classificar_sessao(sessao.codigo)
+                prof_id, prof_obj, prof_tipo = (None, None, None)
+                if is_pesq and sessao.paciente.pesquisador_responsavel:
+                    prof_id = sessao.paciente.pesquisador_responsavel.id_pessoa
+                    prof_obj = profissionais.get(prof_id)
+                    if isinstance(prof_obj, Pesquisador) and not prof_obj.restricoes_pesquisador.esta_disponivel(dia_horario):
+                        avisos.append("Conflito de horário para o pesquisador.")
+                elif is_fisio and sessao.paciente.fisioterapeuta_responsavel:
+                    prof_id = sessao.paciente.fisioterapeuta_responsavel.id_pessoa
+                    prof_obj = profissionais.get(prof_id)
+                    if isinstance(prof_obj, Fisioterapeuta) and not prof_obj.restricoes_fisioterapeuta.esta_disponivel(dia_horario):
+                        avisos.append("Conflito de horário para o fisioterapeuta.")
 
-                # Checar disponibilidade do Fisioterapeuta
-                if is_fisio_session and sessao.paciente.fisioterapeuta_responsavel:
-                    fisio = profissionais.get(sessao.paciente.fisioterapeuta_responsavel.id_pessoa)
-                    if not fisio.restricoes_fisioterapeuta.esta_disponivel(dia_horario):
-                         avisos.append(f"Conflito de horário para o fisioterapeuta.")
+                if prof_id and agenda_profissional.get(prof_id, {}).get(dia_horario, 0) > 1:
+                    avisos.append("Profissional com outra sessão neste horário.")
 
-                # Checar disponibilidade do Pesquisador
-                if is_pesquisador_session and sessao.paciente.pesquisador_responsavel:
-                    pesq = profissionais.get(sessao.paciente.pesquisador_responsavel.id_pessoa)
-                    if not pesq.restricoes_pesquisador.esta_disponivel(dia_horario):
-                        avisos.append(f"Conflito de horário para o pesquisador.")
+            paciente_sessoes = sessoes_por_paciente[paciente.id_pessoa]
+            indice_sessao = self._enumerar_sessao(sessao.codigo)
+            print(paciente_sessoes) #dbg
+
+            if sessao.codigo.startswith('S') and dia_horario and indice_sessao > 0:
+                previous_session = paciente_sessoes[indice_sessao - 1]
+                if previous_session.dia:
+                    delta = sessao.dia - previous_session.dia
+                    if delta.days <= 0:
+                        avisos.append("Agendado no mesmo dia ou antes da sessão anterior.")
+                    elif delta.days < 4:
+                            avisos.append("Agendado em menos de 4 dias após a sessão anterior.")
+                    elif 14 < delta.days:
+                            avisos.append("Agendado em mais de 14 dias após sessão anterior.")
+
+            if sessao.codigo.startswith('F') and dia_horario:
+                sessao_inicial = paciente_sessoes[0]
+                if sessao_inicial and sessao_inicial.dia:
+                    delta_f = sessao.dia - sessao_inicial.dia
+                    if sessao.codigo == 'F00':
+                        if delta_f.days < 60:
+                            avisos.append("Primeiro follow-up agendado menos de 60 dias após a sessão inicial.")
+                        elif 120 < delta_f.days:
+                            avisos.append("Primeiro follow-up agendado mais de 120 dias após a sessão inicial.")
+                    if sessao.codigo == 'F01':
+                        if delta_f.days < 150:
+                            avisos.append("Segundo follow-up agendado menos de 150 dias após a sessão inicial.")
+                        elif 210 < delta_f.days:
+                            avisos.append("Segundo follow-up agendado mais de 210 dias após a sessão inicial.")
 
             card = SessaoCard(self, sessao, avisos=avisos)
             card.pack(fill="x", expand=True, padx=10, pady=5)
@@ -166,8 +219,6 @@ class AgendaPessoaWidget(ctk.CTkFrame):
             if sessao.dia and sessao.horario:
                 dia_horario = datetime.combine(sessao.dia, sessao.horario)
 
-            print("sessao enviada {} {}: {} {}".format(sessao.paciente.id_pessoa, sessao.codigo, sessao.status_agendamento, sessao.conclusao)) #dbg
-
             payload.append({
                 'id_sessao': sessao.id_sessao,
                 'dia_horario': dia_horario.isoformat() if dia_horario else None,
@@ -183,9 +234,7 @@ class AgendaPessoaWidget(ctk.CTkFrame):
         else:
             try:
                 if agendamento_pendente:
-                    print("agenda widget pre wrapper")
                     wrapper()
-                    print("agenda widget post wrapper")
             except Exception as e:
                 CTkMessagebox(title="Erro", message="Ocorreu um erro no auto-agendamento das sessões", icon="cancel")
             else:
